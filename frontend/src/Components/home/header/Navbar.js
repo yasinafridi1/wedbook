faa13e65b7344cdbb58d5db37f46bb7298b7d643
yasinafridi11 <khialyyasin@gmail.com>
2022-09-26 10:05:51 +0500

    Navbar customization

--- frontend/src/Components/home/header/Navbar.js
+++ frontend/src/Components/home/header/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className='nav fixed top-0 w-full flex justify-center items-center flex-col' >
-            <div className='w-full flex justify-between items-center py-2'>
+            <div className='w-full flex justify-between items-center py-1'>
                 <div className='flex justify-between items-center w-1/3 ml-10 flex-col md:flex-row'>
                     <a href='tel:+923045760623' className='mt-3 flex justify-start items-center transition-all ease-in-out duration-500 hover:text-pink-600'>
                         <i className="fa-solid fa-phone text-xs xsm:text-base md:text-lg font-bold"></i>
@@ -16,29 +16,24 @@ const Navbar = () => {
                     </a>
                 </div>
                 <div className='pr-2 xsm:pr-10'>
-                    <div className=''>
+                    <div>
                         <a href="https://www.facebook.com" className='pr-2'> <i className="fa-brands fa-square-facebook text-2xl transition-all ease-in-out duration-300 hover:text-blue-600"></i></a>
                         <a href="https://www.instagram.com" className='px-2'><i className="fa-brands fa-square-instagram text-2xl transition-all ease-in-out duration-300 hover:text-pink-700"></i></a>
                         <a href="https://www.twitter.com" className='px-2'>  <i className="fa-brands fa-square-twitter text-2xl transition-all ease-in-out duration-300 hover:text-blue-400"></i></a>
                     </div>
                 </div>
             </div>
-            <div className='w-full py-3 flex justify-between items-center'>
-                <div className='flex flex-between pl-2 sm:pl-3 items-center w-2/3 sm:w-1/2'>
+            <div className='w-full py-1 flex justify-between items-center'>
+                <div className='flex flex-between pl-2 sm:pl-3 items-center w-2/3 sm:w-1/3'>
                     <div className='flex w-1/2 xsm:pl-4'>
-                        <i className="fa-solid fa-bag-shopping px-2 text-2xl pl-6 text-pink-800" ></i>
-                        <h2 className='text-base pt-1 xsm:text-xl font-bold text-pink-700'>Shoplify</h2>
+                        <h2 className='text-xl pt-1 xsm:text-3xl font-bold text-yellow-300 header-text'>WedBook</h2>
                     </div>
-                    <a to="/cart" className="w-1/2 flex justify-end xsm:mr-8 sm:mr-0">
-                        <i className="fa-solid fa-cart-shopping text-2xl text-pink-800 hover:text-pink-600 transition ease-in-out duration-400"></i>
-                        <p className='text-xs font-bold'>10</p>
-                    </a>
                 </div>
-                <ul className=' hidden pr-1 xl:w-1/2 sm:flex sm:justify-evenly sm:w-1/2'>
-                    <li><a to="/" className='pl-8 xl:text-xl lg:text-lg md:text-base sm:text-xs sm:font-semibold hover:text-pink-600 transition ease-in-out duration-500'>Home</a></li>
-                    <li><a to="/products" className='xl:text-xl lg:text-lg md:text-base sm:pl-5 sm:text-xs sm:font-semibold hover:text-pink-600 transition ease-in-out duration-500'>Products</a></li>
-                    <li> <a href="#about" className='xl:text-xl lg:text-lg md:text-base sm:pl-5 sm:text-xs sm:font-semibold hover:text-pink-600 transition ease-in-out duration-500'>About</a> </li>
-                    <li> <a href="#contact" className='xl:text-xl lg:text-lg md:text-base sm:pl-5 sm:text-xs sm:font-semibold hover:text-pink-600 transition ease-in-out duration-500 mr-2 lg:mr-0'>Contact</a> </li>
+                <ul className='hidden pr-1 xl:w-1/2 sm:flex sm:justify-evenly w-1/2'>
+                    <li><a href="/" className='pl-8 xl:text-xl lg:text-lg md:text-base sm:text-xs sm:font-bold hover:text-pink-600 transition ease-in-out duration-500'>Home</a></li>
+                    <li><a href="/products" className='xl:text-xl lg:text-lg md:text-base sm:pl-5 sm:text-xs sm:font-bold hover:text-pink-600 transition ease-in-out duration-500'>Products</a></li>
+                    <li> <a href="#about" className='xl:text-xl lg:text-lg md:text-base sm:pl-5 sm:text-xs sm:font-bold hover:text-pink-600 transition ease-in-out duration-500'>About</a> </li>
+                    <li> <a href="#contact" className='xl:text-xl lg:text-lg md:text-base sm:pl-5 sm:text-xs sm:font-bold hover:text-pink-600 transition ease-in-out duration-500 mr-2 lg:mr-0'>Contact</a> </li>
                 </ul>
                 <input type="checkbox" id="openSidebarMenu" />
                 <label htmlFor="openSidebarMenu" className="sidebarIconToggle pr-4 sm:hidden">
