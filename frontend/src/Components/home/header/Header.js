41cc0f4b892bc7813b0efbb3aa5cc7326521ae91
yasinafridi11 <khialyyasin@gmail.com>
2022-09-26 11:07:07 +0500

    Header part completed

--- frontend/src/Components/home/header/Header.js
+++ frontend/src/Components/home/header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Slider from './Slider';
 
 const Header = () => {
     return (
         <header className='header'>
             <Navbar />
+            <Slider />
         </header>
     );
 }
--- frontend/src/Components/home/header/Slider.js
+++ frontend/src/Components/home/header/Slider.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Slider = () => {
     return (
-        <div>
-
+        <div className='flex justify-center items-center flex-col h-full'>
+            <h2 className='text-4xl font-bold text-white'>Your Wedding, Your Way</h2>
+            <h3 className='text-2xl text-white mt-3'>Welcome to <span className='header-text font-bold text-green-500'>WedBook</span></h3>
+            <h5 className='text-xl text-white mt-2 '>Find the best wedding vendors with thousands of trusted reviews</h5>
         </div>
     );
 }
